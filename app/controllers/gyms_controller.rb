b0d8eb2d0e2643164786c8de9b09d1fbe5e51315
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-07 17:32:26 +0100

    authorize, not forget when we use pundit, also check out the _form, maybe we mess up that  ¨

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -12,6 +12,7 @@ class GymsController < ApplicationController
 
   def new
     @gym = Gym.new
+    authorize @gym
 
   end
 
@@ -23,12 +24,37 @@ class GymsController < ApplicationController
   def create
     @gym =  Gym.new(params[:gym])
     @gym.save
+    @gym.user = current_user
 
+    if @gym.save
+      redirect_to @gym, notice: 'your gym was create,thanks!'
+    else
+      render :new
+    end
   end
 
   def upadate
     @gym = Gym.find(params[:id])
     @gym.update(params[:gym])
+
+    if @gym.update(gym_params)
+      redirect_to @gym, notice: 'your gym was updated, thanks!'
+    else
+      render :edit
+  end
+end
+
+  def destroy
+    #wacth out with this method
+    @gym.destroy
+    redirect_to :gyms_path
+
+  end
+
+  private
+
+  def gym_params
+    params.require(:gym).permit(:name, :description, :pricing, :user_id)
 
   end
 end
