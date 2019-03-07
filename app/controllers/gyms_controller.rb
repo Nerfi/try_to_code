2ef012c96f15e49fc3818a09131b9b8827f28fe9
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-07 16:40:23 +0100

    configuriin gym controller

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -4,4 +4,31 @@ class GymsController < ApplicationController
     @gyms =  Gym.all
 
   end
+
+  def show
+    @gym = Gym.find(paramas[:id])
+
+  end
+
+  def new
+    @gym = Gym.new
+
+  end
+
+  def edit
+    @gym = Gym.find(params[:id])
+
+  end
+
+  def create
+    @gym =  Gym.new(params[:gym])
+    @gym.save
+
+  end
+
+  def upadate
+    @gym = Gym.find(params[:id])
+    @gym.update(params[:gym])
+
+  end
 end
