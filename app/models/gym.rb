d1601e849b0249d8cf6dbfbb56848e1aaf7c05d6
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-08 18:47:19 +0100

    improved valitaions

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,5 +2,6 @@ class Gym < ApplicationRecord
   belongs_to :user
   validates :name, presence: true , uniqueness: true
   validates :description, presence: true
+  validates :description, length: { minimum: 15 }
   validates :pricing, presence: true
 end
