9e246cbdfb99bcc17ccbaf5159469bfccb4babf0
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-08 18:45:40 +0100

    added some validations on the gym model

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,3 +1,6 @@
 class Gym < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true , uniqueness: true
+  validates :description, presence: true
+  validates :pricing, presence: true
 end
